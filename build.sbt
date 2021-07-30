4b83a91d75bce99581cb0ecc2a2765b8db4c5caa
Maxim Karvonen <Maxim.Karvonen@gmail.com>
2021-07-30 22:47:32 +0100

    Add common JSON types
    Port the "standard" type hierarchy (includig Undefined) from Scala 2.
    
    The types move inside Json object to simplify the access. The Json
    object becomes a new entry point for all the functionality (types,
    serialization, parsing). Specific json types could be accessed via
    Json.Type notation (i.e. Json.String, Json.Null and so on). This is one
    extra dot in the record but one less import compared to JsonNull,
    JsonString, etc... The specific json types should rarely be used in the
    code so it is an improvement.
    
    The Json is now also type of json value (it was JsonValue in the
    previous version). This also reduces amount of imports needed.
    
    An exception was also moved inside the Json object for the same reason
    of simplicity.

--- build.sbt
+++ build.sbt
@@ -8,6 +8,16 @@ val commonSettings = Seq(
   scalaVersion := "3.0.1"
 )
 
+val libJsonClassic = project.in(file("json/classic"))
+  .settings(commonSettings)
+  .settings(
+    name := "json-classic",
+    description := 
+      """Easy to use and lightweight json parsing/generation library.
+      It provides javascript-like member access syntax and set of handy conversions.
+      The library is Scala 3 version of lib-json library in Scala 2."""
+  )
+
 val root = project.in(file("."))
   .settings(commonSettings)
   .settings(
@@ -15,3 +25,4 @@ val root = project.in(file("."))
     description := 
       """An (opinionated) set of small modular (faceted) libraries for the most common tasks"""
   )
+  .aggregate(libJsonClassic)
---
+++ json/classic/src/Json.scala
@@ -0,0 +1,175 @@
+package io.github.maxkar
+package json.classic
+
+import java.io.IOException
+
+import scala.language.dynamics
+
+
+/** A real JSON value or a placeholder for absent (non-existing) value.  */
+abstract sealed class Json extends Dynamic:
+  /**
+   * Retrieves a child of this value considering it is a json object. Returns
+   * {{{Json.Undefined}}} if this value is not an object or the name is not defined
+   * on the current value.
+   * @param name field name.
+   * @return field of the object or {{{Json.Undefined}}} when key not found or this 
+   *   value is not json object.
+   */
+  def selectDynamic(name: java.lang.String): Json = Json.Undefined
+
+
+  /**
+   * Synonym for the {{{selectDynamic}}} method. Useful when key is not
+   * valid scala identifier or when index access is used after field access.
+   */
+  inline def apply(name: java.lang.String): Json =
+    selectDynamic(name)
+
+
+  /**
+   * Selects a value by index. Returns {{{Json.Undefined}} if this value is not a
+   * {{{JsonValue.Array}}} or if the index is out of bounds. 
+   * @param idx index of the value to select.
+   * @return value at the given index or {{{Json.Undefined}}} when this value is not
+   *   an array or index is out of bounds.
+   */
+  def apply(idx: Int): Json = Json.Undefined
+
+
+  /**
+   * Converts this value into the target type (if possible).
+   * @throws Json.Exception if value could not be parsed as the target type.
+   */
+  inline def as[T](using conv: Conversion[Json, T]): T =
+    conv(this)
+
+end Json
+
+
+
+/**
+ * The main entry point to the json-related functionality. 
+ * Contains specific json types and provides access to parsing and serialization functionality.
+ */
+object Json:
+  /**
+   * Exception in JSON handling or processing. This exception is only raised
+   * by parsing and conversion functions (and never by JSON generation functions).
+   * Json manipulations usually occur as part of input and output hence the 
+   * inheritance from IOException.
+   */
+  class Exception(msg: java.lang.String) extends IOException(msg) 
+
+
+  /**
+   * Undefined (non-existing) value. It is returned from member selectors when
+   * a field does not exist on an object or index is out of array bounds. Object and
+   * Array constructors ignore undefined elements. A json {{{null}}} literal is 
+   * considered to be some defined value and is represented by the {{{Null}}} constant.
+   */
+  case object Undefined extends Json
+
+
+  /**
+   * JSON null literal. Unlike {{{Undefined}}} this is a real value which is
+   * could be read or written.
+   */
+  case object Null extends Json
+
+
+  /** JSON `true` literal. */
+  case object True extends Json
+
+
+  /** JSON `false` literal. */
+  case object False extends Json
+
+
+  /**
+   * JSON string literal. The value is an unquoted (effective) string.
+   * @param value value of the literal.
+   */
+  case class String(v: java.lang.String) extends Json
+
+
+  /**
+   * JSON number literal. It contains unparsed (raw) string representation of
+   * a number (the representation has to be well-formed). Client code could
+   * decide how numbers should be converted (with or without precision loss)
+   * depending on the context.
+   */
+  case class Number(value: java.lang.String) extends Json:
+    Number.validate(value)
+
+
+  /** 
+   * JSON array (a sequence of JSON values). The instance constructor does not
+   * accept {{{Undefined}}} values, use {{{Json.array}}} if this may be the case.
+   * @param elements elements of the array.
+   */
+  case class Array(elements: Seq[Json]) extends Json:
+    if elements.contains(Undefined) then
+      throw new IllegalArgumentException("Json array could not contain Undefined elements")
+
+    override def apply(idx: Int): Json =
+      if idx < 0 || idx >= elements.length then
+        Undefined
+      else
+        elements(idx)
+
+
+    /** Concatenates this array with elements from another array. */
+    def ++(other: Array): Array = Array(elements ++ other.elements)
+
+  end Array
+
+
+  /**
+   * JSON object (an unordered collection of key-value pairs). The instance constructor
+   * does not accept {{{Undefined}}} values, use {{{Json.make}}} if this may be the case.
+   * @param values map from keys to corresponding values.
+   */
+  final case class Object(values: Map[java.lang.String, Json]) extends Json:
+    if values.values.iterator.contains(Undefined) then
+      throw new IllegalArgumentException("Undefined values are not allowed in Json object")
+
+    override def selectDynamic(name: java.lang.String): Json =
+      values.getOrElse(name, Undefined)
+
+    /** Combines two objects into one. */
+    def ++(other: Object): Object = Object(values ++ other.values)
+
+  end Object
+
+
+  /** Utilities for json number. */
+  object Number:
+    /** Optional minus sign. */
+    private val patMaybeMinus = "-?"
+    /** Pattern for the JSON integer value part. */
+    private val patInt = "0|([1-9]\\d*)"
+    /** Patters for the fractional part. */
+    private val patFrac = "\\.\\d+"
+    /** Pattern for the exponent part. */
+    private val patExp = "[eE][-+]?\\d+"
+
+    /** Pattern for the json number. */
+    private val pat = 
+      java.util.regex.Pattern.compile(
+        s"${patMaybeMinus}(${patInt})(${patFrac}?)(${patExp}?)"
+      )
+
+
+    /** 
+     * Validates that the number is a valid JSON number representation.
+     * @throws IllegalArgumentException if value is not valid.
+     */
+    private def validate(num: java.lang.String): Unit =
+      if !pat.matcher(num).matches then
+        /* Json parser explicitly raises JsonException, this one is always developer error so IAE. */
+        throw new IllegalArgumentException("Invalid JSON number value " + num)
+
+  end Number
+
+end Json
