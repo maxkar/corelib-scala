3454c4652c534b9dedf3b4ff4267680ee4588953
Maxim Karvonen <Maxim.Karvonen@gmail.com>
2022-11-09 13:57:41 +0000

    Rename coroutine type parameter
    `Op` is good name for encoding "operation", better than `Tok`

--- fun/src/coroutine/Coroutine.scala
+++ fun/src/coroutine/Coroutine.scala
@@ -11,26 +11,26 @@ import fun.typeclass.Monad
  * Instances of this class just capture data and delegate implementation to the
  * static Coroutine (apart from monad instance which explicitly caches result).
  * Although it is possible to just use methods and APIs of the `Coroutine` object,
- * using an instance (capturing Tok type) may be more convenient.
+ * using an instance (capturing Op type) may be more convenient.
  *
- * @tparam Tok (super-)type encoding an "external" process.
+ * @tparam Op (super-)type encoding an "external" process.
  */
-final class Coroutine[Tok[_]]:
+final class Coroutine[Op[_]]:
   /** The type representing routine being executed with coroutine support. */
-  type Routine[T] = Coroutine.Routine[Tok, T]
+  type Routine[T] = Coroutine.Routine[Op, T]
 
   /** Result of running the routine. */
-  type RunResult[T] = Coroutine.RunResult[Tok, T]
+  type RunResult[T] = Coroutine.RunResult[Op, T]
 
   /**
    * Creates a new suspension represented by the given token.
    * @param token token describing "what" should be done externally to the routine.
    */
-  inline def suspend[T](token: Tok[T]): Routine[T] =
+  inline def suspend[T](token: Op[T]): Routine[T] =
     Coroutine.suspend(token)
 
   /** Monad implementation for the routine. */
-  given monadInstance: Monad[Routine] = Coroutine.monadInstance[Tok]
+  given monadInstance: Monad[Routine] = Coroutine.monadInstance[Op]
 
 
   /** Runs the monad and returns either the final result or suspension description. */
@@ -43,17 +43,17 @@ end Coroutine
  * A basic coroutine implementation.
  *
  * The coroutine use some form of "typed" tokens to encode coroutines and then builds
- * execution around the concept. The Token[T] encodes some "external process" that will
+ * execution around the concept. The Open[T] encodes some "external process" that will
  * provide value of the type T.
  */
 object Coroutine:
   /**
    * Result of running some routine with a token-based suspension.
-   * @tparam Tok (super-) type of the token representing an external operation. The
+   * @tparam Op (super-) type of the token representing an external operation. The
    *   actual type's argument is the value that is supposed to be returned from the
    *   corresponding operation.
    */
-  enum RunResult[Tok[_], T]:
+  enum RunResult[Op[_], T]:
     /**
      * Routine evaluation finished with the given outcome.
      * @param result result of the routine evalutaion.
@@ -63,64 +63,64 @@ object Coroutine:
 
     /**
      * The execution was suspended (with the given token) for an external operation.
-     * @tparam Tok (super-)type of the token representing an external operation.
+     * @tparam Op (super-)type of the token representing an external operation.
      * @tparam Cor specific type that is "returned" by the external operation.
      * @tparam T type of the whole routine (not the external operation).
      * @param token token encoding the requested operation.
      * @param continue function to resume routine evalutaion after value is available
      *   from some external source.
      */
-    case Suspended[Tok[_], Cor, T](
-          token: Tok[Cor],
-          continue: Cor => Routine[Tok, T],
-        ) extends RunResult[Tok, T]
+    case Suspended[Op[_], Cor, T](
+          token: Op[Cor],
+          continue: Cor => Routine[Op, T],
+        ) extends RunResult[Op, T]
   end RunResult
 
 
   /**
    * Routine - something that could be evaluated (with some processes
    * being executed outside).
-   * @tparam Tok (super-)type used to encode external procesess.
+   * @tparam Op (super-)type used to encode external procesess.
    * @tparam V type that is produced by the routine.
    */
-  enum Routine[Tok[_], +V]:
+  enum Routine[Op[_], +V]:
     /** Just a value. */
     case Pure(value: V)
 
     /** Standard monadic flatmap operation. */
-    case FlatMap[Tok[_], V, R](
-          base: Routine[Tok, V],
-          fn: V => Routine[Tok, R]
-        ) extends Routine[Tok, R]
+    case FlatMap[Op[_], V, R](
+          base: Routine[Op, V],
+          fn: V => Routine[Op, R]
+        ) extends Routine[Op, R]
 
     /** Suspension with the given token. */
-    case Suspend[Tok[_], V](token: Tok[V]) extends Routine[Tok, V]
+    case Suspend[Op[_], V](token: Op[V]) extends Routine[Op, V]
   end Routine
 
 
 
   /**
    * Supsends the execution with the given token.
-   * @tparam Tok general (super-)type of all the suspension tokens.
+   * @tparam Op general (super-)type of all the suspension tokens.
    * @tparam T type returned by the process encoded by the token.
    * @param token token describing the "external" process yielding value of type T.
    */
-  inline def suspend[Tok[_], T](token: Tok[T]): Routine[Tok, T] =
+  inline def suspend[Op[_], T](token: Op[T]): Routine[Op, T] =
     Routine.Suspend(token)
 
 
   /** Monad implementation for the routine type. */
-  given monadInstance[Tok[_]]: Monad[({type M[T] = Routine[Tok, T]})#M] with
-    override def pure[T](v: T): Routine[Tok, T] =
+  given monadInstance[Op[_]]: Monad[({type M[T] = Routine[Op, T]})#M] with
+    override def pure[T](v: T): Routine[Op, T] =
       Routine.Pure(v)
 
-    override def bind[S, R](v: Routine[Tok, S], fn: S => Routine[Tok, R]): Routine[Tok, R] =
+    override def bind[S, R](v: Routine[Op, S], fn: S => Routine[Op, R]): Routine[Op, R] =
       Routine.FlatMap(v, fn)
   end monadInstance
 
 
   /** Runs the routine until it completes or until it requests to perform an external operation. */
-  def run[Tok[_], T](routine: Routine[Tok, T]): RunResult[Tok, T] =
+  def run[Op[_], T](routine: Routine[Op, T]): RunResult[Op, T] =
     var cur = routine
     while true do
       cur match
